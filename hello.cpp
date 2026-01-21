9a16a193923969790728ad34da7f5e25bab4d5e8
lolol90000 <157571910+lolol90000@users.noreply.github.com>
2026-01-21 10:26:49 -0800

    commit

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+// CSCI 27X - GitHub Upload Practice
+// Name: Diesel Wilson
+// Date: 2026-1-20
+// Section: CSCI 271
+#include <iostream>
+using namespace std;
+int main() {
+cout << "Hello from GitHub! My first commit works." << endl;
+return 0;
+}
